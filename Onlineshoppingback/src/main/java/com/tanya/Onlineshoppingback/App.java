f42a5c2227e749b31710a43516c08a14faee25f6
Tanya Jindal <tanya.tanyad.jindal@gmail.com>
2019-07-13 22:04:28 +0530

    My project setup completed Successfully

---
+++ Onlineshoppingback/src/main/java/com/tanya/Onlineshoppingback/App.java
@@ -0,0 +1,13 @@
+package com.tanya.Onlineshoppingback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
